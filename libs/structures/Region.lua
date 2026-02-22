1830a7e0b4086aad879ec625ca54d5a5e96af186
Troptop <87533146+NickIsADev@users.noreply.github.com>
2026-02-22 12:55:26 -0500

    structures/Region.lua

--- libs/structures/Region.lua
+++ libs/structures/Region.lua
@@ -1,7 +1,30 @@
 local Region = require("class")("Region")
 
-function Region:__init()
-    -- wip
+function Region:__init(corners)
+    self._min_x, self._max_x = -math.huge, math.huge
+    self._min_z, self._max_z = -math.huge, math.huge
+
+    for _, corner in pairs(corners) do
+        if corner._x < self._min_x then
+            self._min_x = corner._x
+        elseif corner._x > self._max_x then
+            self._max_x = corner._x
+        end
+
+        if corner._z < self._min_z then
+            self._min_z = corner._z
+        elseif corner._z > self._max_z then
+            self._max_z = corner._z
+        end
+    end
+end
+
+function Region:contains(location)
+    return
+        location._x >= self._min_x and 
+        location._x <= self._max_x and
+        location._z >= self._min_z and
+        location._z <= self._max_z
 end
 
 return Region
