df41cb613b583b6bd4fab871f88e338caad47564
Troptop <87533146+NickIsADev@users.noreply.github.com>
2026-02-22 19:35:31 -0500

    bump to v3.0.0

--- package.lua
+++ package.lua
@@ -1,6 +1,6 @@
 return {
     name = "NickIsADev/erlua",
-    version = "3.0.0-dev",
+    version = "3.0.0",
     description = "A library providing dynamic ratelimiting, custom functions, and easy access to the ER:LC API.",
     license = "MIT",
     files = {
